5c7fe8b36dec64a2f911cda19be59de37894f81e
David Alsh <alshdavid@gmail.com>
2019-09-05 23:38:37 +1000

    array support

--- src/defaults.ts
+++ src/defaults.ts
@@ -6,6 +6,6 @@ export type QueryProcessor = (state: any, ...args: any[]) => any
 export const QueryAction = 'QUERY_EXEC'
 
 export const DefaultProcessor = (state: any, fn: any) => {
-  const update = { ...state, ...fn(state) }
+  const update = Object.assign(state, fn(state))
   return update
 }
